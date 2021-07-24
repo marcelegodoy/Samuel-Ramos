aee3410b92c3f29fad47eefd090f74aff8a504c0
Marcele Godoy <marcelegodoy@gmail.com>
2021-07-24 11:30:02 -0300

    Make projects#index the root_path for user logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+  get '/user', to: "admin/projects#index", as: :user_root
 
   namespace :admin do
     resources :projects, only: [:index]
     resources :texts, only: [:index]
     resources :tags, only: [:index]
+
+    root to: 'projects#index'
   end
 end
