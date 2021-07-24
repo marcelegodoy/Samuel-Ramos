0638171c88b3e4248faa2f7d7ba33e5421e74d14
Marcele Godoy <marcelegodoy@gmail.com>
2021-07-24 12:27:42 -0300

    Create redirect for user after login based in it's role

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_user if current_user
+  end
+
+  def redirect_user
+    redirect_to current_user.admin? ? admin_projects_path : guest_projects_path
   end
 end
