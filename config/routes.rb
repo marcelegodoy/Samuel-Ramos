e1b52afe3ab5d21bedb1c68a1f856608aa2fdad9
Marcele Godoy <marcelegodoy@gmail.com>
2021-07-23 21:20:32 -0300

    Create routes to edit models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  namespace :admin do
+    resources :projects, only: [:index]
+    resources :texts, only: [:index]
+    resources :tags, only: [:index]
+  end
 end
