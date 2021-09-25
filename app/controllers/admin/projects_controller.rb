6256f143606f85df01d727393d6fe8b6c1a56cde
Marcele Godoy <marcelegodoy@gmail.com>
2021-09-25 17:06:11 -0300

    Define all methods for projects inside projects controller

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,7 +1,53 @@
 module Admin
   class ProjectsController < BaseController
+    before_action :set_project, only: %i[show create edit update]
+
     def index
       @projects = Project.all
+    end
+
+    def show;end
+
+    def new
+      @project = Project.new
+    end
+
+    def create
+      @project = Project.new(project_params)
+      if @project.save
+        redirect_to project_path(@project), notice: 'Projeto salvo com sucesso'
+      else
+        render :show
+      end
+    end
+
+    def edit;end
+
+    def update
+      @project.update(project_params)
+      if @project.save
+        redirect_to admin_project_path(@project), notice: 'Projeto editado com sucesso'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      if @project.destroy
+        redirect_to admin_projects_path, notice: 'Projeto deletado com sucesso'
+      else
+        render :index
+      end
+    end
+
+    private
+
+    def set_project
+      @project = Project.new
+    end
+
+    def project_params
+      params.require(:project).permit(:description, :title)
     end
   end
 end
