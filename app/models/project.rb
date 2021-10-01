46b9ff15660d1fc3ef08ea41dbf20974c5cecb3a
Marcele Godoy <marcelegodoy@gmail.com>
2021-09-30 23:22:55 -0300

    Change relation to receive more than one image

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   validates_presence_of :title, :description
 
-  has_one_attached :image, dependent: :destroy
+  has_many_attached :images, dependent: :destroy
 end
